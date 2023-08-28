dfd769e27bb5cd9c24c79f4db98b7a5c468adea8
soywiz <soywiz@gmail.com>
2023-08-28 12:52:23 +0200

    Test that

--- korge-core/test/common/korlibs/ffi/WASMLibTest.kt
+++ korge-core/test/common/korlibs/ffi/WASMLibTest.kt
@@ -10,11 +10,19 @@ class WASMLibTest {
         init { finalize() }
     }
 
+    object SimpleWASM2 : WASMLib("AGFzbQEAAAABBwFgAn9/AX8DAgEABAUBcAEBAQUDAQAABhQDfwBBCAt/AUGIgAILfwBBiIACCwcQAgNzdW0AAAZtZW1vcnkCAAkGAQBBAQsACgoBCAAgACABag8L".fromBase64()) {
+        val sum: (Int, Int) -> Int by func()
+        init { finalize() }
+    }
+
     @Test
     fun test() {
         assertEquals(3, SimpleWASM.sum(1, 2))
-        val ptr = SimpleWASM.allocBytes(byteArrayOf(0, 10, 20, 30))
-        SimpleWASM.freeBytes(ptr)
+        assertEquals(3, SimpleWASM2.sum(1, 2))
+        assertEquals(5, SimpleWASM.sum(2, 3))
+        assertEquals(5, SimpleWASM2.sum(2, 3))
+        //val ptr = SimpleWASM.allocBytes(byteArrayOf(0, 10, 20, 30))
+        //SimpleWASM.freeBytes(ptr)
         //assertEquals(listOf(10, 20), SimpleWASM.readBytes(ptr + 1, 2).map { it.toInt() }.toList())
     }
 }
